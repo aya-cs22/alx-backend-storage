eb9b1998c0fa2377d78e9ebcade9f04d520b2813
aya-cs22 <aya786930@gmail.com>
2024-08-10 19:14:15 +0300

    a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. Note: An average score can be a decimal

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,9 @@
 -- a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes
 -- and store the average score for a student. Note: An average score can be a decimal
 DELIMITER $$;
-CREATE IF NOT EXISTS PROCEDURE ComputeAverageScoreForUser(user_id int)
+CREATE  PROCEDURE ComputeAverageScoreForUser(user_id int)
 BEGIN
-    SELECT AVG(scores)
+    SELECT AVG(scores) AS average_score
     FROM scores
     WHERE user_id = user_id
 END;$$
