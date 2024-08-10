9d009aa8a2efdb882282bfd1c3ea88597dde9d93
aya-cs22 <aya786930@gmail.com>
2024-08-10 18:21:39 +0300

    a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,12 +1,22 @@
 -- a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
-DELIMITER $$ ;
-CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM projects WHERE name = project_name) THEN
-        INSERT INTO projects (name) VALUES (project_name);
-    END IF;
-    INSERT INTO corrections (user_id, project_name, score)
-    VALUES (user_id, project_id, score);
-END $$
-DELIMITER ;
+-- DELIMITER $$ ;
+-- CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score INT)
+-- BEGIN
+--     IF NOT EXISTS (SELECT 1 FROM projects WHERE name = project_name) THEN
+--         INSERT INTO projects (name) VALUES (project_name);
+--     END IF;
+--     INSERT INTO corrections (user_id, project_name, score)
+--     VALUES (user_id, project_id, score);
+-- END $$
+-- DELIMITER ;
 -- poiuytrewq321#@
+DELIMITER $$;
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
+BEGIN
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+END;$$
+DELIMITER ;
