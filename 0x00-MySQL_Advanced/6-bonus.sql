a6d9b0dc8ccb6dab879b724ee5c04eb20dedd4ca
aya-cs22 <aya786930@gmail.com>
2024-08-10 18:25:59 +0300

    a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,6 +1,9 @@
 -- a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
-DELIMITER $$ ;
-CREATE PROCEDURE AddBonus (user_id int, project_name varchar(255), score int)
+-- DELIMITER $$ ;
+-- CREATE PROCEDURE AddBonus (user_id int, project_name varchar(255), score int)
+-- BEGIN
+DELIMITER $$;
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
 BEGIN
     IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name) THEN
         INSERT INTO projects (name) VALUES (project_name);
