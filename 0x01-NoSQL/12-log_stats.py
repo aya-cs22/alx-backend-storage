3a97cb8c4e70d14c669e4d39752bbf809534e971
aya-cs22 <aya786930@gmail.com>
2024-08-13 10:39:03 +0300

    a Python script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
+"""Log stats module"""
 from pymongo import MongoClient
-# client = MongoClient('mongodb://localhost:27017/')
-# db = client['logs']
-# collection = db['nginx']
+
+
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs.nginx
 
-    # number of documents in this collection
-    # total_logs = collection.count_documents({})
-    total_logs = db.count_documents({})
-    print(f"{total_logs} logs\n")
-    print("Methods:\n")
-    # number of documents with the method
-    # methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    # for method in methods:
-    #     # method_count = collection.count_documents({'method': method})
-    #     method_count = db.count_documents({'method': method})
-    #     print(f"\tmethod {'method'}: {method_count}")
+    num_logs = db.count_documents({})
+    print(f"{num_logs} logs")
+
     get = db.count_documents({'method': 'GET'})
     post = db.count_documents({'method': 'POST'})
     put = db.count_documents({'method': 'PUT'})
@@ -30,6 +22,28 @@ if __name__ == "__main__":
     print(f"\tmethod PUT: {put}")
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
+
     status = db.count_documents({'method': 'GET', 'path': '/status'})
     print(f"{status} status check")
+# #!/usr/bin/env python3
+# from pymongo import MongoClient
+# # client = MongoClient('mongodb://localhost:27017/')
+# # db = client['logs']
+# # collection = db['nginx']
+# if __name__ == "__main__":
+#     client = MongoClient('mongodb://127.0.0.1:27017')
+#     db = client.logs.nginx
 
+#     # number of documents in this collection
+#     # total_logs = collection.count_documents({})
+#     total_logs = db.count_documents({})
+#     print(f"{total_logs} logs\n")
+#     print("Methods:\n")
+#     # number of documents with the method
+#     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+#     for method in methods:
+#         # method_count = collection.count_documents({'method': method})
+#         method_count = db.count_documents({'method': method})
+#         print(f"\tmethod {'method'}: {method_count}")
+#     status = db.count_documents({'method': 'GET', 'path': '/status'})
+#     print(f"{status} status check")
