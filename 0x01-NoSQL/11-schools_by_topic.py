40747948c8638b105656d8b420b2f41a76ee2103
aya-cs22 <aya786930@gmail.com>
2024-08-12 20:52:42 -0700

    a Python function that returns the list of school having a specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,4 +2,4 @@
 """return the list of school having a specific topic"""
 def schools_by_topic(mongo_collection, topic):
     """a Python function that returns the list of school having a specific topic"""
-   return mongo_collection.find({"topic" : topic})
+   return mongo_collection.find({"topics" : topic})
