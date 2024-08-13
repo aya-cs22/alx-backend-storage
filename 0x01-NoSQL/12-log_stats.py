f81022bc6f9a71d9756771672ec98794e0d41cb5
aya-cs22 <aya786930@gmail.com>
2024-08-13 10:31:58 +0300

    a Python script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 from pymongo import MongoClient
-client = MongoClient('mongodb://localhost:27017/')
-db = client['logs']
-collection = db['nginx']
-# number of documents in this collection
-total_logs = collection.count_documents({})
-print(f"{total_logs} logs\n")
-print("Methods:\n")
-# number of documents with the method
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    method_count = collection.count_documents({'method': method})
-    print(f"\tmethod {'method'}: {method_count}")
-status = collection.count_documents({'method': 'GET', 'path': '/status'})
-print(f"{status} status check")
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client['logs']
+# collection = db['nginx']
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs.nginx
+
+    # number of documents in this collection
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs\n")
+    print("Methods:\n")
+    # number of documents with the method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {'method'}: {method_count}")
+    status = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{status} status check")
