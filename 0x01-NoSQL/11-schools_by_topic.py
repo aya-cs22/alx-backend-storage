928fd12d8b67c936ff066549d476632ef88f9c6b
aya-cs22 <aya786930@gmail.com>
2024-08-13 10:27:19 +0300

    a Python script that provides some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """return the list of school having a specific topic"""
+
+
 def schools_by_topic(mongo_collection, topic):
-    """a Python function that returns the list of school having a specific topic"""
-       return mongo_collection.find({"topics": topic})
+    """a Python function that returns the list of school"""
+    return mongo_collection.find({"topics": topic})
