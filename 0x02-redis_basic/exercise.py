977ac1fa581b4d33819cc28723bec17fb16db9f6
aya-cs22 <aya786930@gmail.com>
2024-08-15 16:07:31 +0300

    Cache class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,12 +3,12 @@ import redis
 import uuid
 from typing import Union
 class Cache:
-    def __init__(self, redis):
+    def __init__(self):
         self._redis = redis
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]):
-        random_uuid = uuid.uuid4()
-        self._redis.set(random_uuid)
-        random_uuid = self._redis.get(random_uuid)
-        return random_uuid
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        random_uuid_key = str(uuid.uuid4())
+        self._redis.set(random_uuid_key, data)
+        # random_uuid = self._redis.get(random_uuid_key, data)
+        return random_uuid_key
