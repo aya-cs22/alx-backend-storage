c46e9ae0a658a9dbfa7bfd5dae738d2af7657f70
aya-cs22 <aya786930@gmail.com>
2024-08-16 07:48:14 +0300

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,25 +24,22 @@ def call_history(method: Callable) -> Callable:
         return result
     return wrapper
 
-def replay(func: Callable):
-    """
-    Prototype: def replay(func: Callable):
-    Displays history of calls of a particular function
-    """
-    r = redis.Redis()
-    key_m = func.__qualname__
-    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
-    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
-    calls_number = len(inp_m)
-    times_str = 'times'
-    if calls_number == 1:
-        times_str = 'time'
-    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
-    print(fin)
-    for k, v in zip(inp_m, outp_m):
-        fin = '{}(*{}) -> {}'.format(
-            key_m, k.decode('utf-8'), v.decode('utf-8'))
-        print(fin)
+def replay(self, method):
+        """
+        display the history of calls of a particular function
+
+        """
+        key = method.__qualname__
+        key_inputs= key + ":inputs"
+        key_outputs= key + ":outputs"
+        count = self.get(key).decode("utf-8")
+        print(f"Cache.{key} was called {count} times:")
+        inputs = self._redis.lrange(key_inputs, 0, -1)
+        outputs = self._redis.lrange(key_outputs, 0, -1)
+        zipped_list = list(zip(inputs, outputs))
+
+        for i, (input, output) in enumerate(zipped_list):
+            print(f"{key} (*{output}) -> {input}")
 
 
 class Cache():
