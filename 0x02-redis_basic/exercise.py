08b03877e53652ab3594044ab061bf3e6db307a2
aya-cs22 <aya786930@gmail.com>
2024-08-16 07:43:33 +0300

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,22 +24,26 @@ def call_history(method: Callable) -> Callable:
         return result
     return wrapper
 
-def replay(method):
-    key = method.__qualname__
-    input_key = f"{key}:inputs"
-    output_key = f"{key}:outputs"
-    
-    # Retrieve all inputs and outputs from Redis
-    inputs = cache._redis.lrange(input_key, 0, -1)
-    outputs = cache._redis.lrange(output_key, 0, -1)
-    
-    # Convert bytes to strings for printing
-    inputs = [input.decode('utf-8') for input in inputs]
-    outputs = [output.decode('utf-8') for output in outputs]
-    
-    print(f"{method.__qualname__} was called {len(inputs)} times:")
-    for args, result in zip(inputs, outputs):
-        print(f"{method.__qualname__}(*{args}) -> {result}")
+def replay(func: Callable):
+    """
+    Prototype: def replay(func: Callable):
+    Displays history of calls of a particular function
+    """
+    r = redis.Redis()
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m, k.decode('utf-8'), v.decode('utf-8'))
+        print(fin)
+
 
 class Cache():
     def __init__(self):
